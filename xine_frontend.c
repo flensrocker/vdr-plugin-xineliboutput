26a52607f47bbb1f20ddcd5d6eaccb6159baa95f
phintuka <phintuka>
2006-08-19 19:30:30 +0000

    *** empty log message ***

--- xine_frontend.c
+++ xine_frontend.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_frontend.c,v 1.12 2006-08-19 19:18:21 phintuka Exp $
+ * $Id: xine_frontend.c,v 1.13 2006-08-19 19:30:30 phintuka Exp $
  *
  */
 
@@ -133,7 +133,7 @@ static double fe_dest_pixel_aspect(fe_t *this, double video_pixel_aspect,
     /* 16:10 */
     case 4:  result = (16.0/10.0 * (double)this->height/(double)this->width); break;
     /* Pan&Scan */
-    case 4: {
+    case 5: {
       double aspect_diff /*= video_pixel_aspect - 1.0*/;
 	      /* TODO */
 	      /* does not work (?) */
@@ -149,7 +149,7 @@ aspect_diff=(video_pixel_aspect*(double)video_width/(double)video_height) - 4.0 
 	      break;
             }
     /* center cut-out */
-    case 5: {
+    case 6: {
 /*#warning center cut-out mode not implemented*/
       break;
     }
