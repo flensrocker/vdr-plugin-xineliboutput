4d45e27758cc9ec58f6581abcc2eda7ce8ea1bce
phintuka <phintuka>
2008-04-28 20:48:05 +0000

    Make some functions/parameters const

--- tools/time_pts.h
+++ tools/time_pts.h
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: time_pts.h,v 1.3 2008-04-20 21:29:29 phintuka Exp $
+ * $Id: time_pts.h,v 1.4 2008-04-28 20:48:05 phintuka Exp $
  *
  */
 
@@ -30,14 +30,14 @@ class cTimePts
   public:
     cTimePts(void);
 
-    int64_t Now(void);
+    int64_t Now(void) const;
     void    Set(int64_t Pts = 0LL);
 
     void Pause(void);
     void Resume(void);
-    void TrickSpeed(int Multiplier);
+    void TrickSpeed(const int Multiplier);
 
-    void SetScrSpeed(int ScrSpeed = 90000);
+    void SetScrSpeed(const int ScrSpeed = 90000);
 };
 
 #endif // __TIME_PTS_H
