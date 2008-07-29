6af71acbe7886a845df5f0cf3bf4a9aaee626aba
phintuka <phintuka>
2008-07-29 21:02:30 +0000

    Make const. Specify sizes of types.

--- vdrlogo_32x32.c
+++ vdrlogo_32x32.c
@@ -4,18 +4,19 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: vdrlogo_32x32.c,v 1.2 2006-12-19 17:27:49 phintuka Exp $
+ * $Id: vdrlogo_32x32.c,v 1.3 2008-07-29 21:02:30 phintuka Exp $
  *
  */
 
 /* GIMP RGBA C-Source image dump (vdrlogo_32x32.c) */
 
-static const struct {
-  unsigned int 	 width;
-  unsigned int 	 height;
-  /*unsigned int 	 bytes_per_pixel; / * 3:RGB, 4:RGBA */ 
-  unsigned char	 pixel_data[32 * 32 * 4 + 1];
-} vdrlogo_32x32 = {
+typedef struct {
+  const uint32_t width;
+  const uint32_t height;
+  const uint8_t  pixel_data[32 * 32 * sizeof(uint32_t) + 1];
+} __attribute__((packed)) sxfe_32x32_icon_t;
+
+static const sxfe_32x32_icon_t vdrlogo_32x32 = {
   32, 32, /*4,*/
   "\375\375\374\377\375\375\375\377\375\375\375\377\375\375\375\377\375\375"
   "\375\377\375\375\375\377\375\375\375\377\375\375\375\377\375\375\375\377"
