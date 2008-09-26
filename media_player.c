ab59fecaac1cf76d26d99510b20e0d5b0112a433
phintuka <phintuka>
2008-09-26 13:25:48 +0000

    1.0.2: Backported bugfixes from CVS trunk - Fixed replaying images from network sources (http://, ...) (Thanks to Rolf Ahrenberg)

--- media_player.c
+++ media_player.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: media_player.c,v 1.52 2008-05-07 13:27:15 phintuka Exp $
+ * $Id: media_player.c,v 1.52.2.1 2008-09-26 13:25:48 phintuka Exp $
  *
  */
 
@@ -1102,7 +1102,10 @@ void cXinelibImagePlayer::Activate(bool On)
 {
   if(On) {
     m_Active = true;
-    cXinelibDevice::Instance().PlayFile(*cString::sprintf("file:%s", *cPlaylist::EscapeMrl(m_File)), 0, true);
+    cXinelibDevice::Instance().PlayFile( ( (*m_File)[0]=='/' 
+					   ? *cString::sprintf("file:%s", *cPlaylist::EscapeMrl(m_File))
+					   : *m_File),
+					0, true);
   } else {
     m_Active = false;
     cXinelibDevice::Instance().PlayFile(NULL, 0);
@@ -1113,7 +1116,10 @@ bool cXinelibImagePlayer::ShowImage(const char *File)
 {
   m_File = File;
   if(m_Active)
-    return cXinelibDevice::Instance().PlayFile(*cString::sprintf("file:%s", *cPlaylist::EscapeMrl(m_File)), 0, true);
+    return cXinelibDevice::Instance().PlayFile( ( (*m_File)[0] == '/' 
+						  ? *cString::sprintf("file:%s", *cPlaylist::EscapeMrl(m_File))
+						  : *m_File ),
+					       0, true);
   return true;
 }
 
