f61f6b79bb576a37777558ff915fbbfb5e07b838
phintuka <phintuka>
2008-11-11 20:48:13 +0000

    Removed include "xine_frontend.h"

--- xine_frontend_lirc.h
+++ xine_frontend_lirc.h
@@ -4,16 +4,16 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_frontend_lirc.h,v 1.1 2008-11-10 23:53:45 phintuka Exp $
+ * $Id: xine_frontend_lirc.h,v 1.2 2008-11-11 20:48:13 phintuka Exp $
  *
  */
 
 #ifndef XINE_FRONTEND_LIRC_H
 #define XINE_FRONTEND_LIRC_H
 
-#include "xine_frontend.h"
+struct fe_s;
 
-void lirc_start(fe_t *fe, char *lirc_dev, int repeat_emu);
+void lirc_start(struct fe_s *fe, char *lirc_dev, int repeat_emu);
 void lirc_stop(void);
 
 #endif /* XINE_FRONTEND_LIRC_H */
