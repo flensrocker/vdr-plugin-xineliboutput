9d2c11e2afc395089f5a9d1accf3ad2f28e80c58
phintuka <phintuka>
2008-11-13 22:58:40 +0000

    Remove deps to xine_frontend_internal.h and xine-lib

--- xine_frontend_lirc.h
+++ xine_frontend_lirc.h
@@ -4,16 +4,16 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_frontend_lirc.h,v 1.2 2008-11-11 20:48:13 phintuka Exp $
+ * $Id: xine_frontend_lirc.h,v 1.3 2008-11-13 22:58:40 phintuka Exp $
  *
  */
 
 #ifndef XINE_FRONTEND_LIRC_H
 #define XINE_FRONTEND_LIRC_H
 
-struct fe_s;
+struct frontend_s;
 
-void lirc_start(struct fe_s *fe, char *lirc_dev, int repeat_emu);
+void lirc_start(struct frontend_s *fe, char *lirc_dev, int repeat_emu);
 void lirc_stop(void);
 
 #endif /* XINE_FRONTEND_LIRC_H */
