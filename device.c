07c6c601f1446bf9d8dc97637ca8263be16191fd
phintuka <phintuka>
2009-06-08 16:13:03 +0000

    Merged revision 1.65   Added TimeoutMs to PlayFile()

--- device.c
+++ device.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: device.c,v 1.57.2.9 2009-06-08 13:40:31 phintuka Exp $
+ * $Id: device.c,v 1.57.2.10 2009-06-08 16:13:03 phintuka Exp $
  *
  */
 
@@ -894,7 +894,8 @@ bool cXinelibDevice::EndOfStreamReached(void)
 }
 
 bool cXinelibDevice::PlayFile(const char *FileName, int Position, 
-			      bool LoopPlay, ePlayMode PlayMode)
+			      bool LoopPlay, ePlayMode PlayMode,
+			      int TimeoutMs)
 {
   TRACEF("cXinelibDevice::PlayFile");
   TRACE("cXinelibDevice::PlayFile(\"" << FileName << "\")");
@@ -910,14 +911,14 @@ bool cXinelibDevice::PlayFile(const char *FileName, int Position,
     for(int i = 0; i < mi_Count; i++) 
       m_MetaInfo[i][0] = 0;
     if(m_server)
-      result = m_server->PlayFile(FileName, Position, LoopPlay, PlayMode);
+      result = m_server->PlayFile(FileName, Position, LoopPlay, PlayMode, TimeoutMs);
     if(m_local)
-      result = m_local->PlayFile(FileName, Position, LoopPlay, PlayMode);
+      result = m_local->PlayFile(FileName, Position, LoopPlay, PlayMode, TimeoutMs);
   } else if(/*!FileName &&*/m_PlayingFile != pmNone) {
     if(m_server) 
-      result = m_server->PlayFile(NULL, 0);
+      result = m_server->PlayFile(NULL, 0, 0, pmNone, TimeoutMs);
     if(m_local)
-      result = m_local->PlayFile(NULL, 0);
+      result = m_local->PlayFile(NULL, 0, 0, pmNone, TimeoutMs);
     if(!m_liveMode)
       SetReplayMode();
     else
