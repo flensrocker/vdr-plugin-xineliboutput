05ab8b96965bcc2ea2b7762cf049e38f97031acf
phintuka <phintuka>
2009-09-27 13:04:48 +0000

    Merged revision 1.101   Reset video size when stream changes.   This should fix VDR OSD when playing DVD with some video resolutions.

--- xine_frontend.c
+++ xine_frontend.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_frontend.c,v 1.53.2.9 2009-09-18 11:40:44 phintuka Exp $
+ * $Id: xine_frontend.c,v 1.53.2.10 2009-09-27 13:04:48 phintuka Exp $
  *
  */
 
@@ -1372,6 +1372,7 @@ static void *fe_control(frontend_t *this_gen, const char *cmd)
     if(this->slave_stream)
       fe_control(this_gen, "SLAVE 0x0\r\n");
     init_dummy_ports(this, 0);
+    this->video_width = this->video_height = 0;
 
   } else if(!strncmp(cmd, "SLAVE 0x", 8)) {
     unsigned long pt;
@@ -1406,6 +1407,7 @@ static void *fe_control(frontend_t *this_gen, const char *cmd)
 	fe_post_rewire(this);
       }
       this->slave_playback_finished = !slave_stream;
+      this->video_width = this->video_height = 0;
     }
 
   } else if(!strncmp(cmd, "ENDOFSTREAM", 11)) {
