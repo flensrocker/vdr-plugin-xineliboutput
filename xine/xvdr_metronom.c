b1ef5eb637b1d631c3e6139ffa9521852f18b473
phintuka <phintuka>
2010-01-26 11:46:33 +0000

    Fixed frame duration for I-frames-only trick speed modes

--- xine/xvdr_metronom.c
+++ xine/xvdr_metronom.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xvdr_metronom.c,v 1.2 2010-01-17 23:03:26 phintuka Exp $
+ * $Id: xvdr_metronom.c,v 1.3 2010-01-26 11:46:33 phintuka Exp $
  *
  */
 
@@ -24,16 +24,16 @@
 static void got_video_frame(metronom_t *metronom, vo_frame_t *frame)
 {
   xvdr_metronom_t *this = (xvdr_metronom_t *)metronom;
-  uint64_t          pts = frame->pts;
+  int64_t          pts  = frame->pts;
 
   this->video_frames++;
 
   if (this->frame_decoded)
     this->frame_decoded(this->handle, this->video_frames, this->audio_frames);
 
-  if (this->trickspeed < 0) {
+  if (this->trickspeed) {
     frame->pts       = 0;
-    frame->duration *= (-this->trickspeed);
+    frame->duration *= 12; /* GOP */
   }
 
   this->orig_metronom->got_video_frame (this->orig_metronom, frame);
