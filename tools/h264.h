c6f3292cee81acf5193ab091200387bdbb481bfb
phintuka <phintuka>
2010-02-02 22:42:51 +0000

    IS_NAL_???(): cast to const

--- tools/h264.h
+++ tools/h264.h
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: h264.h,v 1.9 2009-06-29 15:49:43 phintuka Exp $
+ * $Id: h264.h,v 1.10 2010-02-02 22:42:51 phintuka Exp $
  *
  */
 
@@ -24,9 +24,9 @@ extern "C" {
 
 
 #if defined(__i386__) || defined(__x86_64__)
-#  define IS_NAL_SPS(buf)     (*(uint32_t*)(buf) == 0x07010000U)
-#  define IS_NAL_AUD(buf)     (*(uint32_t*)(buf) == 0x09010000U)
-#  define IS_NAL_END_SEQ(buf) (*(uint32_t*)(buf) == 0x0a010000U)
+#  define IS_NAL_SPS(buf)     (*(const uint32_t *)(buf) == 0x07010000U)
+#  define IS_NAL_AUD(buf)     (*(const uint32_t *)(buf) == 0x09010000U)
+#  define IS_NAL_END_SEQ(buf) (*(const uint32_t *)(buf) == 0x0a010000U)
 #else
 #  define IS_NAL_SPS(buf)     ((buf)[0] == 0 && (buf)[1] == 0 && (buf)[2] == 1 && (buf)[3] == NAL_SPS)
 #  define IS_NAL_AUD(buf)     ((buf)[0] == 0 && (buf)[1] == 0 && (buf)[2] == 1 && (buf)[3] == NAL_AUD)
