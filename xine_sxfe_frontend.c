0d68e879b4744a8e5ce183d125ac9453119a0e2e
phintuka <phintuka>
2010-07-15 12:34:05 +0000

    Request backing store for windows. (patch #3013050, thanks to grueni75)

--- xine_sxfe_frontend.c
+++ xine_sxfe_frontend.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_sxfe_frontend.c,v 1.126 2010-06-02 17:30:23 rofafor Exp $
+ * $Id: xine_sxfe_frontend.c,v 1.127 2010-07-15 12:34:05 phintuka Exp $
  *
  */
 
@@ -1057,24 +1057,33 @@ static double detect_display_ratio(Display *dpy, int screen)
  */
 static void create_windows(sxfe_t *this)
 {
+  XSetWindowAttributes xswa;
+
+  xswa.background_pixel = 0x00000000;
+  xswa.border_pixel     = 0;
+  xswa.backing_store    = WhenMapped;
+
   XLockDisplay(this->display);
+
   /* create and display our video window */
-  this->window[0] = XCreateSimpleWindow (this->display,
-                                         DefaultRootWindow(this->display),
-                                         this->x.xpos, this->x.ypos,
-                                         this->x.width, this->x.height,
-                                         1, 0, 0);
-  this->window[1] = XCreateSimpleWindow(this->display, XDefaultRootWindow(this->display),
-                                        this->xinerama_x, this->xinerama_y,
-                                        this->x.width, this->x.height,
-                                        0, 0, 0);
+
+  this->window[0] = XCreateWindow (this->display, DefaultRootWindow(this->display),
+                                   this->x.xpos, this->x.ypos,
+                                   this->x.width, this->x.height, 1,
+                                   CopyFromParent, InputOutput, CopyFromParent,
+                                   (CWBackPixel | CWBorderPixel | CWBackingStore), &xswa);
+  this->window[1] = XCreateWindow (this->display, DefaultRootWindow(this->display),
+                                   this->xinerama_x, this->xinerama_y,
+                                   this->x.width, this->x.height, 0,
+                                   CopyFromParent, InputOutput, CopyFromParent,
+                                   (CWBackPixel | CWBorderPixel | CWBackingStore), &xswa);
 
   /* full-screen window */
   set_fullscreen_props(this);
 
   /* Window hint */
   XClassHint *classHint = XAllocClassHint();
-  if(classHint) {
+  if (classHint) {
     classHint->res_name = "VDR";
     classHint->res_class = "VDR";
     XSetClassHint(this->display, this->window[0], classHint);
