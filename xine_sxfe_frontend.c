e2aa4b0741d8089a1d79b4350232773069281926
phintuka <phintuka>
2010-09-24 12:31:37 +0000

    Fixed handling of XKeyEvents when XKeysymToString() returns NULL (Thanks to Winfried KÃ¶hler)

--- xine_sxfe_frontend.c
+++ xine_sxfe_frontend.c
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_sxfe_frontend.c,v 1.138 2010-09-12 22:01:43 phintuka Exp $
+ * $Id: xine_sxfe_frontend.c,v 1.139 2010-09-24 12:31:37 phintuka Exp $
  *
  */
 
@@ -1586,6 +1586,8 @@ static void XKeyEvent_handler(sxfe_t *this, XKeyEvent *kev)
     if (fe_event)
       this->x.fe.send_event((frontend_t*)this, fe_event);
     else if (!this->no_x_kbd) {
+      const char *ks_name = XKeysymToString(ks);
+      if (ks_name) {
       char keyname[40] = "";
       if (kev->state & Mod1Mask) {
         strcat(keyname, "Alt+");
@@ -1593,8 +1595,9 @@ static void XKeyEvent_handler(sxfe_t *this, XKeyEvent *kev)
       if (kev->state & ControlMask) {
         strcat(keyname, "Ctrl+");
       }
-      strncat(keyname, XKeysymToString(ks), sizeof(keyname) - 11);
+      strncat(keyname, ks_name, sizeof(keyname) - 11);
       this->x.fe.send_input_event((frontend_t*)this, "XKeySym", keyname, 0, 0);
+      }
     }
   }
 }
