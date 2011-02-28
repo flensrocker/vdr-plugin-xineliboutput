4c950c1302d5040f9b6240e88a1757a3cc2c0465
phintuka <phintuka>
2011-02-28 13:41:08 +0000

    Fixed kbd_start() prototype: use forward-declared struct instead of typedef

--- xine_frontend_kbd.h
+++ xine_frontend_kbd.h
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: xine_frontend_kbd.h,v 1.1 2011-01-23 19:42:08 phintuka Exp $
+ * $Id: xine_frontend_kbd.h,v 1.2 2011-02-28 13:41:08 phintuka Exp $
  *
  */
 
@@ -13,7 +13,7 @@
 
 struct frontend_s;
 
-void kbd_start(frontend_t *fe, int slave_mode);
+void kbd_start(struct frontend_s *fe, int slave_mode);
 void kbd_stop(void);
 
 #endif /* XINE_FRONTEND_KBD_H */
