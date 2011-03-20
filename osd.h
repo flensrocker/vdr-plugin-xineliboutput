cced66afde1913844c7905f3034c522f41c2c09e
phintuka <phintuka>
2011-03-20 23:04:47 +0000

    Added vdr-1.7.17 TrueColor OSD support. Note: Only HUD OSD can display TrueColor OSD.       TrueColor OSD is automatically enabled if all clients use HUD OSD. Note: There's no compression yet, remote users may need to disable TrueColor OSD in plugin setup menu...

--- osd.h
+++ osd.h
@@ -4,7 +4,7 @@
  * See the main source file 'xineliboutput.c' for copyright information and
  * how to reach the author.
  *
- * $Id: osd.h,v 1.5 2007-10-15 00:31:39 phintuka Exp $
+ * $Id: osd.h,v 1.6 2011-03-20 23:04:47 phintuka Exp $
  *
  */
 
@@ -20,11 +20,12 @@ class cXinelibOsdProvider : public cOsdProvider
   protected:
     cXinelibDevice *m_Device;
 
+    virtual bool ProvidesTrueColor(void);
+    virtual cOsd *CreateOsd(int Left, int Top, uint Level);
+
   public:
     cXinelibOsdProvider(cXinelibDevice *Device);
     virtual ~cXinelibOsdProvider();
-
-    virtual cOsd *CreateOsd(int Left, int Top, uint Level);
 
     static void RefreshOsd(void);
 
